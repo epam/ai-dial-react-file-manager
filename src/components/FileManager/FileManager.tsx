f61e6754d6c1018fe29a5718b7559b3425224ac7
denys-kolomiitsev <denys_kolomiitsev@epam.com>
2026-09-24 14:23:46 +0200

    feat: allow to set rowHeight in FileManager via additionalGridOptions

--- src/components/FileManager/FileManager.tsx
+++ src/components/FileManager/FileManager.tsx
@@ -1450,10 +1450,11 @@ export const DialFileManagerView: FC = () => {
 
   const gridAdditionalOptions = useMemo<AgGridOptions<GridRow>>(
     () => ({
-      ...forwardedGridOptions.additionalGridOptions,
-      onCellClicked: cellClickHandler,
+      
       headerHeight: COMPACT_VIEW_HEADER_HEIGHT,
       rowHeight: COMPACT_VIEW_HEADER_HEIGHT,
+      ...forwardedGridOptions.additionalGridOptions,
+      onCellClicked: cellClickHandler,
       rowClass: 'group/grid-row',
       defaultColDef: {
         comparator: baseColumnComparator,
